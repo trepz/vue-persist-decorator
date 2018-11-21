2bf69238c2334067f5c9b00c2fa0e3c112db9673
Robert Worman <tr3ppy@gmail.com>
2018-11-22 09:49:25 +1000

    Fix dummy decorator type errors

--- src/vue-rx-decorators.ts
+++ src/vue-rx-decorators.ts
@@ -4,5 +4,5 @@ import { createDecorator } from 'vue-class-component'
  * Dummy decorator.
  */
 export const Dummy = (): PropertyDecorator => createDecorator((options, key) => {
-	options.props[key] = 'dummy'
+	(options.props || (options.props = {}) as any)[key] = 'dummy'
 })
