41f14fefca40c39ecb37a5720ccb30343a19de30
Robert Worman <RWorman@anglicanchurchsq.org.au>
2018-11-22 15:38:41 +1000

    Pivot

--- tests/decorators.spec.ts
+++ tests/decorators.spec.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import { Dummy } from '../src/vue-rx-decorators'
+import { Dummy } from '../src/vue-persist-decorator'
 
 test('dummy decorator does something', async () => {
 	@Component
