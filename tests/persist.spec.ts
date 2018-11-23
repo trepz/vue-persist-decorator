e88608613efa6099426a7c95f6c7a4518edbdaee
Robert Worman <RWorman@anglicanchurchsq.org.au>
2018-11-23 10:41:28 +1000

    Test that computed property is created

--- tests/persist.spec.ts
+++ tests/persist.spec.ts
@@ -1,2 +1,16 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import { Persist } from '../src/vue-persist-decorator'
+
+@Component
+class Comp extends Vue {
+  @Persist()
+  hello: string = 'hi'
+}
+const comp = new Comp()
+
+test('a computed property is set which matches the name of the data property', () => {
+  const computed = comp.$options.computed
+  expect(computed).toBeDefined()
+  expect(computed!.hello).toBeDefined()
+})
