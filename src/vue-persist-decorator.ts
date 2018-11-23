321c28f9ef593b133c0882d92d4ba327adc7989d
Robert Worman <RWorman@anglicanchurchsq.org.au>
2018-11-23 10:53:50 +1000

    Use 4 space indents

--- src/vue-persist-decorator.ts
+++ src/vue-persist-decorator.ts
@@ -1,19 +1,19 @@
 import { createDecorator } from 'vue-class-component'
 
 export interface PersistOptions {
-  expiry?: Date
-  key?: string
+    expiry?: Date
+    key?: string
 }
 
 export const Persist = (options: PersistOptions = {}): PropertyDecorator => {
-  return createDecorator((opts, k) => {
-    const { key = `${opts.name}_${k}`, expiry } = options
-    if (typeof opts.computed !== 'object') opts.computed = Object.create(null)
-    ;(opts.computed as any)[k] = {
-      get() {
-        return ''
-      },
-      set() {}
-    }
-  })
+    return createDecorator((opts, k) => {
+        const { key = `${opts.name}_${k}`, expiry } = options
+        if (typeof opts.computed !== 'object') opts.computed = Object.create(null)
+        ;(opts.computed as any)[k] = {
+            get() {
+                return ''
+            },
+            set() {},
+        }
+    })
 }
--- tests/persist.spec.ts
+++ tests/persist.spec.ts
@@ -4,13 +4,13 @@ import { Persist } from '../src/vue-persist-decorator'
 
 @Component
 class Comp extends Vue {
-  @Persist()
-  hello: string = 'hi'
+    @Persist()
+    hello: string = 'hi'
 }
 const comp = new Comp()
 
 test('a computed property is set which matches the name of the data property', () => {
-  const computed = comp.$options.computed
-  expect(computed).toBeDefined()
-  expect(computed!.hello).toBeDefined()
+    const computed = comp.$options.computed
+    expect(computed).toBeDefined()
+    expect(computed!.hello).toBeDefined()
 })
