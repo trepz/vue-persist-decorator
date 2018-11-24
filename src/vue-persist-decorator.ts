6fafb0724a6e2eb367bbe74ed90bfb5742b7c66a
Robert Worman <tr3ppy@gmail.com>
2018-11-24 11:06:40 +1000

    Add default value option

--- src/vue-persist-decorator.ts
+++ src/vue-persist-decorator.ts
@@ -3,15 +3,18 @@ import { createDecorator } from 'vue-class-component'
 export interface PersistOptions {
     expiry?: Date
     key?: string
+    default?: any
 }
 
 export const Persist = (options: PersistOptions = {}): PropertyDecorator => {
     return createDecorator((opts, k) => {
         const name = (opts.name || '_').toLowerCase()
-        const { key = `${name}_${k}`, expiry } = options
+        const { key = `${name}_${k}`, default: defaultValue, expiry } = options
 
         // Create an empty computed object if one doesn't exist in options already
-        if (typeof opts.computed !== 'object') opts.computed = Object.create(null)
+        if (typeof opts.computed !== 'object') {
+            opts.computed = Object.create(null)
+        }
 
         // Create getter and setter
         ;(opts.computed as any)[k] = {
