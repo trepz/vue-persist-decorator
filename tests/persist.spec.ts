6983f70dac0f20cdb1ee9c7d79e73cb1db8410c3
Robert Worman <tr3ppy@gmail.com>
2018-11-24 12:05:25 +1000

    Add component factory function for easier testing with options

--- tests/persist.spec.ts
+++ tests/persist.spec.ts
@@ -1,24 +1,28 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import { Persist } from '../src/vue-persist-decorator'
+import { Persist, PersistOptions } from '../src/vue-persist-decorator'
 import { LocalStorageMock } from './mocks/localstorage.mock'
 
 global.localStorage = new LocalStorageMock()
 
-@Component
-class Comp extends Vue {
-    @Persist()
-    hello!: string
+const factory = (options?: PersistOptions) => {
+    @Component
+    class Comp extends Vue {
+        @Persist(options)
+        hello!: string
+    }
+    return new Comp()
 }
-const comp = new Comp()
 
 test('a computed property is set which matches the name of the data property', () => {
+    const comp = factory()
     const computed = comp.$options.computed
     expect(computed).toBeDefined()
     expect(computed!.hello).toBeDefined()
 })
 
 test('setting the property stores in localStorage', () => {
+    const comp = factory()
     comp.hello = 'hi'
     expect(localStorage.store.comp_hello).toBe('hi')
 })
