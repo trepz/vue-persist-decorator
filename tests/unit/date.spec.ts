c545e1102285d2e30dd1a99bcbbf5eb4385027e1
Robert Worman <RWorman@anglicanchurchsq.org.au>
2018-11-28 08:10:32 +1000

    Override date class

--- tests/unit/date.spec.ts
+++ tests/unit/date.spec.ts
@@ -1,4 +1,16 @@
 import { parseRelativeDate } from '../../src/vue-persist-decorator'
+declare var global: any
+
+/**
+ * Override date constructor to return a consistent date for diffing.
+ */
+const constantDate = new Date('2018-05-30T07:30:00')
+global.Date = class extends Date {
+    constructor() {
+        super()
+        return constantDate
+    }
+}
 
 describe('Date parsing function', () => {
     test('it is a function', () => {
