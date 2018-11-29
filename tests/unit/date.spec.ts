5f139bbfcb7bdcff0fd42065d7009cc69993d141
Robert Worman <RWorman@anglicanchurchsq.org.au>
2018-11-29 14:29:46 +1000

    Add unit tests for relative time

--- tests/unit/date.spec.ts
+++ tests/unit/date.spec.ts
@@ -5,7 +5,22 @@ const now = Date.now()
 Date.now = jest.fn().mockReturnValue(now)
 
 describe('Date parsing function', () => {
-    test('it is a function', () => {
-        expect(typeof parseRelativeDate).toBe('function')
+    test('it returns the correct relative time', () => {
+        let time
+
+        time = parseRelativeTime('5000ms')
+        expect(time - now).toBe(1000 * 5)
+
+        time = parseRelativeTime('5s')
+        expect(time - now).toBe(1000 * 5)
+
+        time = parseRelativeTime('10m')
+        expect(time - now).toBe(1000 * 60 * 10)
+
+        time = parseRelativeTime('2h')
+        expect(time - now).toBe(1000 * 60 * 60 * 2)
+
+        time = parseRelativeTime('221d')
+        expect(time - now).toBe(1000 * 60 * 60 * 24 * 221)
     })
 })
