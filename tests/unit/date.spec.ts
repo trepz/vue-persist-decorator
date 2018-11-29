7265c5a4980f1a54e27d4cb1337d6e8559f5f244
Robert Worman <RWorman@anglicanchurchsq.org.au>
2018-11-29 14:26:54 +1000

    Mock date.now instead of constructor

--- tests/unit/date.spec.ts
+++ tests/unit/date.spec.ts
@@ -1,17 +1,8 @@
 import { parseRelativeTime } from '../../src/vue-persist-decorator'
 declare var global: any
 
-/**
- * Override date constructor to return a consistent date for diffing.
- */
-const ODate = Date
-const constantDate = new Date('2018-05-30T07:30:00.000Z')
-global.Date = class extends Date {
-    constructor(b: any) {
-        super(b)
-        if (!b) return constantDate
-    }
-}
+const now = Date.now()
+Date.now = jest.fn().mockReturnValue(now)
 
 describe('Date parsing function', () => {
     test('it is a function', () => {
